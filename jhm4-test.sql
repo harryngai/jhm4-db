9eb630cd934054f05dabd81372203c3843e75991
Harry Ngai <160995592+harryngai@users.noreply.github.com>
2025-05-17 04:46:25 +0000

    update

--- jhm4-test.sql
+++ jhm4-test.sql
@@ -12,3 +12,5 @@ INSERT INTO STUDENT (SID, SNAME, SEX, DOB, CLASS) VALUES
 ('S001', '陳大文', 'M', '2006-02-15', '4A'),
 ('S002', '李小明', 'M', '2005-11-23', '4B'),
 ('S003', '王美麗', 'F', '2006-05-30', '4A');
+
+DELETE FROM STUDENT WHERE `SID` = 'S001';
