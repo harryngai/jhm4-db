c030cffe2fa079c7380a2476c04bdb1a6a8ba2a1
Harry Ngai <160995592+harryngai@users.noreply.github.com>
2025-05-24 02:05:15 +0000

    update

--- jhm4-test.sql
+++ jhm4-test.sql
@@ -1,16 +1,14 @@
 CREATE DATABASE jhm4_test;
 USE jhm4_test;
-CREATE TABLE STUDENT (
-    SID   VARCHAR(10) PRIMARY KEY,
-    SNAME VARCHAR(50),
-    SEX   CHAR(1),
-    DOB   DATE,
-    CLASS VARCHAR(10)
-);
- 
-INSERT INTO STUDENT (SID, SNAME, SEX, DOB, CLASS) VALUES
-('S001', '陳大文', 'M', '2006-02-15', '4A'),
-('S002', '李小明', 'M', '2005-11-23', '4B'),
-('S003', '王美麗', 'F', '2006-05-30', '4A');
 
-DELETE FROM STUDENT WHERE `SID` = 'S001';
+CREATE TABLE Student (
+    student_id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    gender VARCHAR(10),
+    birthday DATE
+);
+
+INSERT INTO Student (student_id, name, gender, birthday) VALUES
+(1, '陳大文', 'M', '2006-02-15'),
+(2, '李小明', 'M', '2005-11-23'),
+(3, '王美麗', 'F', '2006-05-30');
